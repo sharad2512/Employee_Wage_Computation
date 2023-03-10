e649a5e9f26839e9e84378b2aea842ea59dfae7c
sharad2512 <shnarute95@gmail.com>
2023-03-10 14:39:07 +0530

    ADDED : Code to calculate employee wage for month

--- src/com/bridgelabz/empwage/EmployeeWage.java
+++ src/com/bridgelabz/empwage/EmployeeWage.java
@@ -5,21 +5,26 @@ public class EmployeeWage {
         final int WAGE_PER_HRS = 20;
         final int IS_PART_TIME = 1;
         final int IS_FULL_TIME = 2;
+        final int NUM_OF_WORKING_DAY = 20;
         int empWage = 0;
         int empHrs = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
-                break;
+        int totalEmpWage = 0;
+        int empCheck = 0;
+        for (int day = 1; day <= NUM_OF_WORKING_DAY; day++) {
+            empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * WAGE_PER_HRS;
+            totalEmpWage = totalEmpWage + empWage;
         }
-        empWage = empHrs * WAGE_PER_HRS;
-        System.out.println("Employee Wage Is " + empWage);
+        System.out.println("Employee Wage Is " + totalEmpWage);
     }
 }
