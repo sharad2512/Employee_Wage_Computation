a6049dafc02c92765d283a02527505ce411c40ab
sharad2512 <shnarute95@gmail.com>
2023-03-10 13:57:30 +0530

    Welcome to employee wage computtion programs

---
+++ src/com/bridgelabz/empwage/EmployeeWage.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.empwage;
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome message to employee wage computation program");
+    }
+}
